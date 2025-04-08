5a60a5cb5e2c8d26566a84b481bf1711f086b238
Marshall Arnold <memers101@users.noreply.github.com>
2025-04-08 03:01:38 -0700

    Step 13 commits

--- asciiArt.js
+++ asciiArt.js
@@ -1,3 +1,12 @@
 const { argv } = require('node:process')
+var figlet = require("figlet")
 
-console.log(argv[2])
+figlet(argv[2], function (err,data) {
+    if (err) {
+        console.log("Error detected.")
+        console.dir(err)
+        return
+    }
+    console.log(data)
+})
+
